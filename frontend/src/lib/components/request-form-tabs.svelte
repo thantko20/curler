4b65a857de5afa0dc9e1677eedfff9e3e25c229e
Thant Ko Zaw <thantmkou10@gmail.com>
2024-07-25 23:01:30 +0630

    wip

--- frontend/src/lib/components/request-form-tabs.svelte
+++ frontend/src/lib/components/request-form-tabs.svelte
@@ -2,6 +2,16 @@
 	import * as Tabs from "./ui/tabs"
 	import curlStore from "$lib/curl-store"
 	import KeyValueTable from "./key-value-table.svelte"
+	import * as Select from "./ui/select"
+	import CodeEditor from "./code-editor.svelte"
+	let bodySelected = {
+		label: "None",
+		value: "none"
+	}
+	let textBodySelected = {
+		label: "JSON",
+		value: "json"
+	}
 </script>
 
 <Tabs.Root value="params" class="overflow-auto p-2">
@@ -34,5 +44,39 @@
 			pairs={Object.entries($curlStore.headers)}
 			onPairChange={curlStore.onHeaderEntryChange}
 		/>
+	</Tabs.Content>
+	<Tabs.Content value="body">
+		<div class="flex gap-2">
+			<Select.Root bind:selected={bodySelected}>
+				<Select.Trigger class="w-36">
+					<Select.Value placeholder="Select an option" />
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value="none">None</Select.Item>
+					<Select.Item value="text">Text</Select.Item>
+					<Select.Item value="form">Form</Select.Item>
+				</Select.Content>
+			</Select.Root>
+			{#if bodySelected.value === "text"}
+				<Select.Root bind:selected={textBodySelected}>
+					<Select.Trigger class="w-36">
+						<Select.Value placeholder="Select an option" />
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Item value="raw">Raw</Select.Item>
+						<Select.Item value="json">JSON</Select.Item>
+						<Select.Item value="yaml">YAML</Select.Item>
+					</Select.Content>
+				</Select.Root>
+			{/if}
+		</div>
+		<div class="mt-4">
+			{#if bodySelected.value === "text"}
+				<CodeEditor output={{ body: "", type: textBodySelected.value }} />
+			{/if}
+			{#if bodySelected.value === "form"}
+				<KeyValueTable pairs={[]} onPairChange={() => {}} />
+			{/if}
+		</div>
 	</Tabs.Content>
 </Tabs.Root>
